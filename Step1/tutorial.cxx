71eb36715a7a2c4e9006735d100ebb139124be6c
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2014-05-25 00:47:17 +0800

    Step 1 - 中文化

--- Step1/tutorial.cxx
+++ Step1/tutorial.cxx
@@ -2,22 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "TutorialConfig.h"
+#include "教學設定.h"
 
 int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
-    fprintf(stdout,"%s Version %d.%d\n",
+    fprintf(stdout,"%s 版本 %d.%d\n",
             argv[0],
             Tutorial_VERSION_MAJOR,
             Tutorial_VERSION_MINOR);
-    fprintf(stdout,"Usage: %s number\n",argv[0]);
+    fprintf(stdout,"使用方式： %s 數字\n",argv[0]);
     return 1;
     }
   double inputValue = atof(argv[1]);
   double outputValue = sqrt(inputValue);
-  fprintf(stdout,"The square root of %g is %g\n",
+  fprintf(stdout,"%g 的平方根為 %g\n",
           inputValue, outputValue);
   return 0;
 }
